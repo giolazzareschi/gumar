e61b3f2354764f6085a172f8bbc03a8594d1c40c
Giordano Bruno Lazzareschi <root@DESKTOP-369R4FA.localdomain>
2021-07-27 20:50:30 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'animate.css/animate.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="infinite">
+          <div className="stars"></div>
+          <div className="stars-2"></div>
+          <div className="twinkling"></div>          
+        </div>
+        <div className="animate__animated animate__fadeIn animate__delay-1s header-wrap">
+          <h1 className="animate__animated animate__bounce animate__delay-4s">Gumar</h1>
+        </div>
+        <div className="animate__animated animate__fadeIn animate__delay-3s start-game-wrap">
+          <button className="button-startg-game">Start Readgame</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
